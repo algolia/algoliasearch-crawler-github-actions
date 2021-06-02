5f3d9af0315bef3fcd93f74aa7aaccab674e4a58
Damien Couchez <damien.couchez@gmail.com>
2021-06-02 15:39:34 +0200

    add fetch import

--- dist/crawler-api-client.js
+++ dist/crawler-api-client.js
@@ -1,5 +1,4 @@
 "use strict";
-/* const fetch = require('node-fetch'); */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -38,6 +37,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CrawlerApiClient = void 0;
+var fetch = require('node-fetch');
 /**
  * Example of class that can be used to hit the Crawler API.
  *
--- src/crawler-api-client.ts
+++ src/crawler-api-client.ts
@@ -1,4 +1,4 @@
-/* const fetch = require('node-fetch'); */
+const fetch = require('node-fetch');
 
 type SearchParams = { [key: string]: string | number | null };
 
