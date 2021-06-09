eca00c52df7ae1be7bf86c35013de79336504427
Damien Couchez <damien.couchez@gmail.com>
2021-06-09 17:10:36 +0200

    chore: add semantic release packages

---
+++ release.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  branches: 'main',
+  verifyConditions: ['@semantic-release/github'],
+  prepare: [
+    {
+      path: '@semantic-release/changelog',
+      changelogFile: 'CHANGELOG.md',
+    },
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: ['package.json', 'CHANGELOG.md'],
+      message:
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
+  publish: ['@semantic-release/github'],
+  success: [],
+  fail: [],
+  npmPublish: false,
+};
