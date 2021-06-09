85eff03fd823d9732fab2c54456f56ed1fddab8e
Damien Couchez <damien.couchez@gmail.com>
2021-06-09 16:08:54 +0200

    gitignore cached files from node_modules

--- node_modules/jsdoc-type-pratt-parser/dist/src/result/IntermediateResult.d.ts
+++
@@ -1,7 +0,0 @@
-import { KeyValueResult, NonTerminalResult } from './NonTerminalResult';
-import { TerminalResult } from './TerminalResult';
-export interface ParameterList {
-    type: 'JsdocTypeParameterList';
-    elements: Array<KeyValueResult | TerminalResult>;
-}
-export declare type IntermediateResult = NonTerminalResult | ParameterList;
